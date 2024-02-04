4d2314975de26321e6d7445f82811cba80b1aad0
iumex <123763828+iumex@users.noreply.github.com>
2024-02-04 23:02:46 +0100

    Update tetris.bat

--- Tetris/tetris.bat
+++ Tetris/tetris.bat
@@ -1,1 +1,1 @@
-clang.exe -o tetris.exe .\tetris.c .\drawing.c .\checkCollision.c .\lineCheck.c -l raylibdll
+clang.exe -o tetris.exe .\tetris.c .\drawing.c .\checkCollision.c .\lineCheck.c .\input.c -l raylibdll
